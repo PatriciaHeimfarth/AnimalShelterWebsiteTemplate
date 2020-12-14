24284a0bc2e640a1ee645b10d10810caa68541c0
Patti85 <pbeier24@googlemail.com>
2020-12-14 19:47:45 +0100

    navbar added

--- frontend/animalshelter-app/src/App.js
+++ frontend/animalshelter-app/src/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      
+
+      <Router>
         <div className="container">
-          <h2>Animals</h2>
+          <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Animalshelter</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                  <li class="nav-item active">
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+                  <button class="btn btn-outline-success" type="submit">Suche</button>
+                </form>
+              </div>
+            </div>
+          </nav>
         </div>
-      
+      </Router>
     );
   }
 }

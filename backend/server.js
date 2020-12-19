acd20f53278fd790fa269843a0e07847525ecaee
Patti85 <pbeier24@googlemail.com>
2020-12-19 18:13:33 +0100

    login method backend

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,19 @@ const cors = require("cors");
 const mongoose = require('mongoose');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
 const DIR = '../frontend/animalshelter-app/public/images/';
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 let Animal = require('./models/animal');
 
 app.listen(port, () => console.log("[Server] online " + new Date()));
@@ -111,4 +120,45 @@ mongoose.connection.once('open', function () {
     console.log('Error', err);
 })
 
+
+//Login Function
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+  username: String,
+  password: String
+});
+
+UserDetail.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+passport.use(UserDetails.createStrategy());
+
+passport.serializeUser(UserDetails.serializeUser());
+passport.deserializeUser(UserDetails.deserializeUser());
+
+const connectEnsureLogin = require('connect-ensure-login');
+
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local',
+  (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (!user) {
+      return res.redirect('/login?info=' + info);
+    }
+
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+
+      return res.redirect('/');
+    });
+
+  })(req, res, next);
+});
+
+UserDetails.register({username:'paul', active: false}, 'paul')
 

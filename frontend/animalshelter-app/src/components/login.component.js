7d51175b6428eed25c34a843d0d2d9a26435c4e0
Patti85 <pbeier24@googlemail.com>
2020-12-19 13:56:44 +0100

    login form

--- frontend/animalshelter-app/src/components/login.component.js
+++ frontend/animalshelter-app/src/components/login.component.js
@@ -4,7 +4,19 @@ class loginComponent extends Component {
     render() {
         return (
             <div>
-                Hello Login!
+                <form  action="/login" method="post">
+                    <div className="form-group">
+                        <label>Username:</label>
+                        <input type="text" name="username" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Password:</label>
+                        <input type="password" name="password" className="form-control"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Log In" className="form-control" />
+                    </div>
+                </form>
             </div>
         );
     }

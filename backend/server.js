c130b95122d012b62d56790b501346b2ada99514
Patti85 <pbeier24@googlemail.com>
2020-12-21 11:55:58 +0100

    new backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
 
@@ -8,8 +8,8 @@ const cors = require("cors");
 const mongoose = require('mongoose');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+const jwt = require('jsonwebtoken');
+const utils = require('./utils');
 
 
 const DIR = '../frontend/animalshelter-app/public/images/';
@@ -17,8 +17,7 @@ const DIR = '../frontend/animalshelter-app/public/images/';
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use(passport.initialize());
-app.use(passport.session());
+
 
 
 let Animal = require('./models/animal');
@@ -63,7 +62,7 @@ app.get("/animals/dogs", function (req, res) {
 });
 
 app.get("/animals", function (req, res) {
-    Animal.find(function(err, animals) {
+    Animal.find(function (err, animals) {
         if (err) {
             console.log(err);
         } else {
@@ -73,8 +72,8 @@ app.get("/animals", function (req, res) {
 });
 
 
-app.post('/animals/add', upload.single('Image'), (req, res, next) => {  
-    console.log(req.file) 
+app.post('/animals/add', upload.single('Image'), (req, res, next) => {
+    console.log(req.file)
     connectEnsureLogin.ensureLoggedIn();
     const animal = new Animal({
         _id: new mongoose.Types.ObjectId(),
@@ -85,7 +84,7 @@ app.post('/animals/add', upload.single('Image'), (req, res, next) => {
         IsEmergencyCase: req.body.IsEmergencyCase,
         Image: DIR + req.file.filename
     });
-     
+
     animal.save().then(result => {
         res.status(201).json({
             message: "New animal successfully created!",
@@ -104,7 +103,7 @@ app.post('/animals/add', upload.single('Image'), (req, res, next) => {
     })
 })
 
- 
+
 //TODO: Delete Route
 
 // Database
@@ -119,45 +118,75 @@ mongoose.connection.once('open', function () {
 
 //Login Function
 
-const Schema = mongoose.Schema;
-const UserDetail = new Schema({
-  username: String,
-  password: String
-});
+const userData = {
+    username: "admin",
+    password: "123456"
+};
 
-UserDetail.plugin(passportLocalMongoose);
-const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
-passport.use(UserDetails.createStrategy());
+app.use(function (req, res, next) {
+    var token = req.headers['authorization'];
+    if (!token) return next();
 
-passport.serializeUser(UserDetails.serializeUser());
-passport.deserializeUser(UserDetails.deserializeUser());
-
-const connectEnsureLogin = require('connect-ensure-login');
-
-app.get('/login', function (req, res) {
-    res.send({ msg: "Hello Dogs" });
-});
-
-app.post('/login', (req, res, next) => {
-    passport.authenticate('local',
-    (err, user, info) => {
-      if (err) {
-        return next(err);
-      }
-  
-      if (!user) {
-        return res.redirect('/login?info=' + info);
-      }
-  
-      req.logIn(user, function(err) {
+    token = token.replace('Bearer ', '');
+    jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
         if (err) {
-          return next(err);
+            return res.status(401).json({
+                error: true,
+                message: "Invalid user."
+            });
+        } else {
+            req.user = user;
+            next();
         }
-  
-        return res.redirect('/');
-      });
-  
-    })(req, res, next);
-  });
-  
+    });
+});
+
+ app.post('/users/signin', function (req, res) {
+    const user = req.body.username;
+    const password = req.body.password;
+
+     if (!user || !password) {
+        return res.status(400).json({
+            error: true,
+            message: "Username or Password required."
+        });
+    }
+
+     if (user !== userData.username || password !== userData.password) {
+        return res.status(401).json({
+            error: true,
+            message: "Username or Password is not correct."
+        });
+    }
+
+    const token = utils.generateToken(userData);
+    const userObj = utils.getCleanUser(userData);
+    return res.json({ user: userObj, token });
+});
+
+
+app.get('/verifyToken', function (req, res) {
+    var token = req.body.token || req.query.token;
+    if (!token) {
+        return res.status(400).json({
+            error: true,
+            message: "Token is required."
+        });
+    }
+    jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+        if (err) return res.status(401).json({
+            error: true,
+            message: "Invalid token."
+        });
+
+        if (user.userId !== userData.userId) {
+            return res.status(401).json({
+                error: true,
+                message: "Invalid user."
+            });
+        }
+        var userObj = utils.getCleanUser(userData);
+        return res.json({ user: userObj, token });
+    });
+});
 

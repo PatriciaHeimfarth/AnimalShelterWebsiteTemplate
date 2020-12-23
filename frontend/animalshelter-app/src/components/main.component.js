ffd0288298c410b6ec8a9b331497ef4cd376a343
Patti85 <pbeier24@googlemail.com>
2020-12-23 12:38:30 +0100

    set custom image

--- frontend/animalshelter-app/src/components/main.component.js
+++ frontend/animalshelter-app/src/components/main.component.js
@@ -58,7 +58,7 @@ export default class Main extends Component {
 
                     <div class="row">
                         <div class="col-lg-4">
-                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><image width="100%" height="100%"  href="https://mdn.mozillademos.org/files/6457/mdn_logo_only_color.png"/></svg>
 
                             <h2>Heading</h2>
                             <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>

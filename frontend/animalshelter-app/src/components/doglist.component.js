221cd34886a9aa41414ab899d35be6d5be2c8473
Patti85 <pbeier24@googlemail.com>
2020-12-29 11:35:13 +0100

    showing deteletion th only when loggedin

--- frontend/animalshelter-app/src/components/doglist.component.js
+++ frontend/animalshelter-app/src/components/doglist.component.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import {  Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import axios from 'axios';
- 
+
 
 const Dog = props => (
     <tr>
@@ -11,15 +11,15 @@ const Dog = props => (
         <td><img src={props.dog.Image} height="100"></img></td>
         <td>{props.dog.Description}</td>
         <td>{props.dog.Birthdate}</td>
-        <td>{props.dog.IsEmergencyCase.toString() }</td>
-        <td><Link to={"/details/"+props.dog._id}>Details</Link></td>
+        <td>{props.dog.IsEmergencyCase.toString()}</td>
+        <td><Link to={"/details/" + props.dog._id}>Details</Link></td>
     </tr>
 )
 
 export default class DogList extends Component {
     constructor(props) {
         super(props);
-        this.state = {dogs: []};
+        this.state = { dogs: [] };
     }
 
     componentDidMount() {
@@ -29,44 +29,53 @@ export default class DogList extends Component {
                 console.table(this.dogs);
 
             })
-            .catch(function (error){
+            .catch(function (error) {
                 console.log(error);
             })
     }
 
     dogsList() {
-        return this.state.dogs.map(function(currentAnimal, i){
-            if(currentAnimal.Species == "Dog"){
-                if(currentAnimal.Image !== undefined){
-                    currentAnimal.Image = (currentAnimal.Image).substring(18) ;
+        return this.state.dogs.map(function (currentAnimal, i) {
+            if (currentAnimal.Species == "Dog") {
+                if (currentAnimal.Image !== undefined) {
+                    currentAnimal.Image = (currentAnimal.Image).substring(18);
                     console.log(currentAnimal.Image);
                 }
                 return <Dog dog={currentAnimal} key={i} />;
             }
-            
+
         })
     }
 
     render() {
+
+        const isLoggedIn = true;
+        let deleteHeader;
+        if (isLoggedIn) {
+            deleteHeader = <th></th>;
+        }
+
         return (
             <div>
                 <h3>Dog List</h3>
                 <table className="table table-striped" style={{ marginTop: 20 }} >
                     <thead>
                         <tr>
-                            <th>Species</th>  
+                            <th>Species</th>
                             <th>Name</th>
                             <th>Image</th>
                             <th>Description</th>
                             <th>Birthdate</th>
                             <th>Emergency?</th>
+                            <th>Details</th>
+                            {deleteHeader}
                         </tr>
                     </thead>
                     <tbody>
-                        { this.dogsList() }
+                        {this.dogsList()}
                     </tbody>
                 </table>
             </div>
         )
-    } 
+    }
 }
